eaaf241d8cb0465412ce39f6130578eda93325c8
Jon Johnson <jonjonsonjr@gmail.com>
2015-01-25 23:30:31 -0500

    fixed css to work properly

--- public/js/views/app.js
+++ public/js/views/app.js
@@ -1,29 +1,31 @@
 var app = app || {};
 
+// Some static things
+var css = ["vendor/bootstrap/dist/css/bootstrap.min.css", "css/style.css"].map(function (url) {
+  return m("link", {
+    href: url,
+    rel: "stylesheet"
+  });
+});
+
+var kill_screen = [
+  m("div.container", [
+    m("div.jumbotron", {style: {marginTop: "200px"}}, [
+      m("h1", "You Died :(")
+    ])
+  ])
+];
+
 app.view = function (ctrl) {
   var user = app.user;
+
   return m("html", [
-    m("head", [
-      m("link", {
-        href: "vendor/bootstrap/dist/css/bootstrap.min.css",
-        rel: "stylesheet"
-      }),
-      m("link", {
-        href: "css/style.css",
-        rel: "stylesheet"
-      })
-    ]),
-    m("body", (user.health() <= 0) ? [
-      m("div", {class: "container"}, [
-        m("div", {class: "jumbotron", style: {"margin-top": "200px"}}, [
-          m("h1", "You Died :(")
-        ])
-      ])
-    ] : [
-      m("div", {class: "container"}, [
-        m("div", {class: "row"}, [
+    m("head", [css]),
+    m("body", (user.health() <= 0) ? kill_screen : [
+      m("div.container", [
+        m("div.row", [
           m("h2", "Amazon"),
-          m("table", {class: "table"}, [
+          m("table.table", [
             m("tr", [
               app.items.filter(function (item, i) {
                 return i < 5;
@@ -32,7 +34,7 @@ app.view = function (ctrl) {
                 return m("td",
                   m("div", [
                     m("h4", item.name),
-                    m("p", m("img", {onclick: ctrl.buyItem.bind(ctrl, item.id), class: "item", src: item.image})),
+                    m("p", m("img.item", {onclick: ctrl.buyItem.bind(ctrl, item.id), src: item.image})),
                     m("strong", {
                       style: {
                         color: item.cost > user.money() ? "#d9534f" : "#000"
@@ -45,10 +47,10 @@ app.view = function (ctrl) {
           ])
         ]),
         m("hr"),
-        m("div", {class: "row"}, [
-          m("div", {class: "col-md-4"}, [
+        m("div.row", [
+          m("div.col-md-4", [
             m("h2", "Stats"),
-            m("table", {class: "table"}, [
+            m("table.table", [
               m("tr", [m("td", "Name"), m("td", user.name())]),
               m("tr", [m("td", "Job"), m("td", user.job().name())]),
               m("tr", [m("td", "Money"), m("td", "$" + user.money())]),
@@ -59,19 +61,18 @@ app.view = function (ctrl) {
               m("tr", [m("td", "Time"), m("td", {"id": "clock"}, app.time.format("h:mm a"))])
             ])
           ]),
-          m("div", {class: "col-md-2"}, [
+          m("div.col-md-2", [
             m("h2", "Job"),
-            m("button", {
+            m("button.work", {
               onclick: ctrl.work,
-              class: "work",
               style: {
-                "background-color": (user.energy() < user.job().difficulty()) ? "#d9534f" : "#fff"
+                backgroundColor: (user.energy() < user.job().difficulty()) ? "#d9534f" : "#fff"
               }
             }, "Work")
           ]),
-          m("div", {class: "col-md-6"}, [
+          m("div.col-md-6", [
             m("h2", "Quests"),
-            m("table", {class: "table"}, [
+            m("table.table", [
               app.user.quests().map(function (quest, i) {
                 return m("tr", [
                   m("td", m("strong", quest.name())),
@@ -81,22 +82,22 @@ app.view = function (ctrl) {
             ])
           ])
         ]),
-        m("div", {class: "row"}, [
-          m("div", {class: "col-md-6"}, [
+        m("div.row", [
+          m("div.col-md-6", [
             m("h2", "My Stuff"),
-            m("table", {class: "table"}, [
+            m("table.table", [
               app.user.items().map(function (item, i) {
                 return m("tr", {onclick: ctrl.sellItem.bind(ctrl, i)}, [
-                  m("td", m("img", {class: "smaller", src: item.image()})),
+                  m("td", m("img.smaller", {src: item.image()})),
                   m("td", m("strong", item.name())),
-                  m("td", m("button", {class: "btn btn-default"}, "Sell"))
+                  m("td", m("button.btn.btn-default", "Sell"))
                 ]);
               })
             ])
           ]),
-          m("div", {class: "col-md-6"}, [
+          m("div.col-md-6", [
             m("h2", "Logs"),
-            m("table", {class: "table"}, [
+            m("table.table", [
               app.logs.map(function (log, i) {
                 return m("tr", [
                   m("td", "Day " + log.day),
